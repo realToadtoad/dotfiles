4d91ff19649b814f4a73156b7b2dd391ca8913bc
realToadtoad <kingdomkeepersguy@gmail.com>
2021-09-26 18:09:21 -0400

    more setup revisions

--- setup.sh
+++ setup.sh
@@ -30,7 +30,10 @@ then
 fi
 
 echo "Setting up chaotic-aur..."
-yay -S chaotic-mirrorlist chaotic-keyring --noconfirm &>/dev/null
+# yay -S chaotic-mirrorlist chaotic-keyring --noconfirm &>/dev/null
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 if ! (cat /etc/pacman.conf | grep chaotic-aur &>/dev/null)
 then
   echo "" | sudo tee -a /etc/pacman.conf
