0b94b2ab8d59485c165eb09d6c617b57e27cce3e
aekaisato <kingdomkeepersguy@gmail.com>
2022-06-23 19:03:45 -0700

    updated kitty config and added script

--- scripts/_template.sh
+++ scripts/_template.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 CONFIG_DIR=$(realpath "$SCRIPT_DIR/../")
+H=~
 
 echo ""
 
---
+++ scripts/kitty.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+CONFIG_DIR=$(realpath "$SCRIPT_DIR/../kitty")
+H=~
+
+echo "Symlinking kitty config..."
+DEST_DIR="$H/.config/kitty"
+rm -rf "$DEST_DIR.old" &>/dev/null
+mv "$DEST_DIR" "$DEST_DIR.old" &>/dev/null
+ln -s "$CONFIG_DIR" "$H/.config/kitty"
+
+echo "Cloning tokyo night theme..."
+git clone https://github.com/davidmathers/tokyo-night-kitty-theme.git "$CONFIG_DIR/tokyo-night-kitty-theme"
