64723ac4f564c2c6f312f17bb8d676ba88f9a644
Aiden Sato <aiden@aekai.dev>
2023-04-30 11:55:28 -0400

    misc updates

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -13,5 +13,8 @@ ln -s "$CONFIG_DIR" "$H/.config/kitty"
 echo "Cloning tokyo night theme..."
 git clone https://github.com/davidmathers/tokyo-night-kitty-theme.git "$CONFIG_DIR/tokyo-night-kitty-theme"
 
-echo "Installing Nerd Fonts..."
-sudo pacman -S ttf-nerd-fonts-symbols-2048-em
+echo "Installing kitty, nerd fonts, fish..."
+sudo pacman -S kitty fish ttf-nerd-fonts-symbols-2048-em
+
+fish -c "set -Ux EDITOR nvim"
+fish -c "fish_config prompt choose scales"
