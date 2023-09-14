605092c8d2739cf5abe669293d37642f3116b29c
Aiden Sato <aiden@aekai.dev>
2023-09-14 09:51:15 -0400

    fix astronvim tokyonight colorscheme

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -2,6 +2,6 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
 
-  { import = "astrocommunity.colorscheme.tokyonight", enabled = true },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
   { import = "astrocommunity.motion.leap-nvim" }
 }
