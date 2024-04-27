7de8a18d30d26401048a3a19a0befe55b01dfdf2
Aiden Sato <aiden@aekai.dev>
2024-04-27 13:19:28 -0400

    update astronvim for new repo

--- scripts/astronvim.sh
+++ scripts/astronvim.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 CONFIG_DIR=$(realpath "$SCRIPT_DIR/../astronvim")
 H=~
 
-sudo pacman -S ttf-nerd-fonts-symbols-2048-em neovim
+sudo pacman -S ttf-nerd-fonts-symbols neovim
 
 rm -rf "$H/.config/nvim.bak" &>/dev/null
 rm -rf "$H/.local/share/nvim.bak" &>/dev/null
 mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.local/share/nvim ~/.local/share/nvim.bak
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 mkdir -p "$H/.config/astronvim/lua"
 ln -s "$CONFIG_DIR" "$H/.config/astronvim/lua/user"
 
