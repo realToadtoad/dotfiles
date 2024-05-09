c7af668db01a27128020e27a9613cf677341eab1
Aiden Sato <aiden.sato@wolterskluwer.com>
2024-05-09 11:16:51 -0400

    astronvim migrate to v4

--- astronvim/plugins/community.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-
-  { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
-  { import = "astrocommunity.motion.leap-nvim" }
-}
--- scripts/astronvim.sh
+++ scripts/astronvim.sh
@@ -10,8 +10,6 @@ rm -rf "$H/.config/nvim.bak" &>/dev/null
 rm -rf "$H/.local/share/nvim.bak" &>/dev/null
 mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.local/share/nvim ~/.local/share/nvim.bak
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
-mkdir -p "$H/.config/astronvim/lua"
-ln -s "$CONFIG_DIR" "$H/.config/astronvim/lua/user"
+ln -s "$CONFIG_DIR" "$H/.config/nvim"
 
 nvim  --headless -c 'quitall'
