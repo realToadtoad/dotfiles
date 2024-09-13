d1e592306090164a722e4a5921f052846c5f422e
aekaisato <35282290+aekaisato@users.noreply.github.com>
2024-09-13 14:01:56 -0400

    better vscode support

--- astronvim/lua/community.lua
+++ astronvim/lua/community.lua
@@ -8,4 +8,5 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.recipes.vscode" },
 }
